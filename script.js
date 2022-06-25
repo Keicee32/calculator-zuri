f6d952c51e5777a4990316aa825aca22977b48e1
Keicee32 <kelss451@gmail.com>
2022-06-25 21:58:37 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+let num1 = Number(prompt('Choose a number'))
+let num2 = Number(prompt('Choose a number'))
+document.getElementById("num1-el").textContent = num1
+document.getElementById("num2-el").textContent = num2
+
+// Create four functions: add(), subtract(), divide(), multiply()
+// Call the correct function when the user clicks on one of the buttons
+// Perform the given calculation using num1 and num2
+// Render the result of the calculation in the paragraph with id="sum-el"
+
+// E.g. if the user clicks on the "Plus" button, you should render
+// "Sum: 10" (since 8 + 2 = 10) inside the paragraph with id="sum-el"
+
+function add(){
+    document.getElementById('sum-el').textContent = num1 + num2
+}
+
+function subtract(){
+    document.getElementById('sum-el').textContent = num1 - num2
+}
+
+function divide(){
+    document.getElementById('sum-el').textContent = num1 / num2
+}
+
+function multiply(){
+    document.getElementById('sum-el').textContent = num1 * num2
+}
